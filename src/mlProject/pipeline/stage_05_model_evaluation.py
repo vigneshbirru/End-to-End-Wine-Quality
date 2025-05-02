7c22ed5c22db08dfa92d0df0281ea7fa2bc1f860
VighneshBirru <vignesh23birru@gmail.com>
2025-05-02 18:55:06 +0530

    app added

---
+++ src/mlProject/pipeline/stage_05_model_evaluation.py
@@ -0,0 +1,16 @@
+from mlProject.config.configuration import ConfigurationManager
+from mlProject.components.model_evaluation import ModelEvaluation
+from mlProject import logger
+
+STAGE_NAME = "Model Evaluation stage"
+
+class ModelEvaluationTrainingPipeline:
+    
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.save_results()
